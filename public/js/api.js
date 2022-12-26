d2a4337a5c4c4236861ae9882d0f69066de60964
SVasconcellos-yssy <stephanye.vasconcellos@yssy.com.br>
2022-12-26 12:51:02 -0300

    netflix

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key= "9054c0226d0e89cf42139fc4a00e3dca"
+let img_url ="https://image.tmdb.org/t/p/w500"
+let genres_list_http ="https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http ="https://api.themoviedb.org/3/discover/movie?"
---
+++ public/js/home.js
@@ -0,0 +1,79 @@
+const main = document.querySelector(".main")
+
+fetch(genres_list_http + new URLSearchParams({
+    api_key: api_key
+}))
+.then(res => res.json())
+.then(data => {
+
+    data.genres.forEach(item => {
+        fetchMovieListByGenres(item.id, item.name)
+    });
+})
+
+const fetchMovieListByGenres = (id, genres) =>{
+    fetch(movie_genres_http + new URLSearchParams({
+        api_key: api_key,
+        with_genres: id,
+        page: Math.floor(Math.random()*3)+1
+    }))
+    .then(resp => resp.json())
+    .then(data =>{
+        makeCategoryElement(`${genres}_movies`, data.results)
+    })
+    .catch(err => console.log(err))
+}
+
+
+const makeCategoryElement = (category, data) =>{
+    main.innerHTML += `
+    <div class="movie-list">
+            <button class="pre-btn">
+                <img src="" alt="previous button">
+            </button>
+
+
+            <h1 class="movie-category">${category.replace("_", " ")}</h1>
+
+            <div class="movie-container" id="${category}">
+              
+                </div>
+            </div>
+
+            <button class="next-btn">
+                <img src="" alt="next  button">
+            </button>
+
+        </div>
+    `
+
+    makeCards(category, data)
+}
+
+
+const makeCards = (id, data) =>{
+    const movieContainer = document.getElementById(id)
+
+    data.forEach((item, i) =>{
+
+        if(item.backdrop_path == null){
+            item.backdrop_path = item.poster_path
+
+            if(item.backdrop_path == null){
+                return
+            }
+        }
+
+        movieContainer.innerHTML += `
+        <div class="movie">
+            <img src="${img_url}${item.backdrop_path}" alt="poster">
+            <p class="movie-title">${item.title}</p>
+        </div>
+        `
+        if( i == data.length -1){
+            setTimeout(() => {
+                setupScrooling()
+            }, 100);
+        }
+    })
+}
